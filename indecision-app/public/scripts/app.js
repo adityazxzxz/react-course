b9dd2aa6b5bd137904e57d1e824cfb31725e6b32
Aditya Pratama <adityapratamax11@gmail.com>
2020-02-15 12:33:51 +0700

    update

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -2,11 +2,30 @@
 
 console.log('app.js is running');
 
-var template = React.createElement(
-  'p',
-  null,
-  'This is JSX from app.jss'
+var templateOne = React.createElement(
+    'p',
+    null,
+    'This is JSX from app.jss'
+);
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Aditya Pratama zxz'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age:17'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location:Tangerang'
+    )
 );
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,6 +1,13 @@
 console.log('app.js is running');
 
-var template = <p>This is JSX from app.jss</p>;
+var templateOne = <p>This is JSX from app.jss</p>;
+var templateTwo = (
+    <div>
+        <h1>Aditya Pratama zxz</h1>
+        <p>Age:17</p>
+        <p>Location:Tangerang</p>
+    </div>
+);
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template,appRoot);
+ReactDOM.render(templateTwo,appRoot);
