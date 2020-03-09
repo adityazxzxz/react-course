1b4c639339aa50ebcc8c961105bc6f6dfa6fb926
Aditya Pratama <adityapratamax11@gmail.com>
2020-03-10 00:09:27 +0700

    create react component

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,44 +1,80 @@
-console.log('app.js is running');
+class IndecisionApp extends React.Component {
+    render(){
+        const title = 'Indecision';
+        const subtitle = 'Put your life in the hands of a computer';
+        const options = ['Thing one','Thing two','Thing three'];
 
-var templateOne = <p>This is JSX from app.jss</p>;
-
-var app = {
-    title:'Indecision app',
-    subtitle:'Put your life in the hands of a computer',
-    options:['one','two']
-};
-
-var template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{app.options.length > 0 ? 'here your options' : 'no options'}</p>
-        <ol>
-            <li>Item one</li>
-            <li>Item two</li>
-        </ol>
-    </div>
-);
-
-function getLocation(location){
-    if(location){
-        return <p>Location:{location}</p>
+        return (
+            <div>
+                <Header title={title} subtitle={subtitle}></Header>
+                <Action></Action>
+                <Options options={options}></Options>
+                <AddOption></AddOption>
+            </div>
+        );
     }
 }
 
-var user = {
-    name:'Aditya Pratamax',
-    age:18,
-    location:'Tangerang'
-};
+class Header extends React.Component {
+    render(){
+        const title = this.props.title;
+        const subtitle = this.props.subtitle;
+        return (
+            <div>
+                <h1>{title}</h1>
+                <h2>{subtitle}</h2>
+            </div>
+        );
+    }
+}
 
-var templateTwo = (
+class Action extends React.Component {
+    render(){
+        return (
+            <div>
+                <button>What should i do?</button>
+            </div>
+        );
+    }
+}
+
+class Options extends React.Component {
+    render() {
+        const options = this.props.options;
+        return (
+            <div>
+                {
+                    options.map((option) => <Option key={option} optionText={option}></Option>)
+                }
+            </div>
+        );
+    }
+}
+
+class Option extends React.Component {
+    render() {
+        const key = this.props.key;
+        const text = this.props.optionText;
+        return (
+            <div>{text}</div>
+        );
+    }
+}
+
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                Add Options Here
+            </div>
+        );
+    }
+}
+
+const jsx = (
     <div>
-        <h1>{user.name ? user.name : 'anonymous'}</h1>
-        {(user.age && user.age >= 18) && <p>Age:{user.age}</p>}
-        {getLocation(user.location)}
+        <IndecisionApp></IndecisionApp>
     </div>
-);
-var appRoot = document.getElementById('app');
+)
 
-ReactDOM.render(templateTwo,appRoot);
+ReactDOM.render(jsx,document.getElementById('app'));
