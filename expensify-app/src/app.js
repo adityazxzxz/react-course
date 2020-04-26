20e1c80bdb90c15bd4f4b50e28df78a600587277
Aditya Pratama <adityapratamax11@gmail.com>
2020-04-26 13:09:47 +0700

    update

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
 
+const store = configureStore();
+store.dispatch(addExpense({description:'Water',amount:2000,createdAt:3000}));
+store.dispatch(addExpense({description:'Gas',amount:5000,createdAt:4000}));
+
+store.dispatch(setTextFilter('ga'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+
+console.log(visibleExpenses);
 
 ReactDOM.render(<AppRouter />,document.getElementById('app'));
--- expensify-app/src/store/configureStore.js
+++ expensify-app/src/store/configureStore.js
@@ -1,8 +1,13 @@
 import { createStore, combineReducers } from 'redux';
+import expensesReducer from '../reducers/expenses';
+import filtersReducer from '../reducers/filters';
 
-const store = createStore(
-    combineReducers({
-        expenses:exspensesReducer,
-        filters:filtersReducer
-    })
-)
+export default () => {
+    const store = createStore(
+        combineReducers({
+            expenses:expensesReducer,
+            filters:filtersReducer
+        })
+    );
+    return store;
+}
