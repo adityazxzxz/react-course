37b8d6441d8fdef6d30979bafeab77746e8fa449
Aditya Pratama <adityapratamax11@gmail.com>
2020-05-02 14:47:54 +0700

    update

--- expensify-app/src/component/ExpensesListFilters.js
+++ expensify-app/src/component/ExpensesListFilters.js
@@ -8,7 +8,7 @@ const ExpensesListFilters = (props) => {
             <input type="text" value={props.filters.text} onChange={(e) => {
                 props.dispatch(setTextFilter(e.target.value))
             }}/>
-            <select onChange={(e) => {
+            <select value={props.filters.sortBy}onChange={(e) => {
                 if(e.target.value === 'date'){
                     props.dispatch(sortByDate())
                 }else if(e.target.value === 'amount'){
