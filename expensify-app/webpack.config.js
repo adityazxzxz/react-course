69f198cdd15d1e69c9ac077521c0b53655721bf5
Aditya <adityapratamax11@gmail.com>
2020-05-18 21:36:36 +0700

    setup production webpack build

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const ExtracTextPlugin = require('extract-text-webpack-plugin');
 
 
 module.exports = (env) => {
     const isProduction = env === 'production';
+    const CSSExtract = new ExtracTextPlugin('styles.css');
 
     return {
         entry: './src/app.js',
@@ -17,13 +19,27 @@ module.exports = (env) => {
                 exclude:/node_modules/
             },{
                 test:/\.s?css$/,
-                use:[
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
+                use:CSSExtract.extract({
+                    use:[
+                        {
+                            loader:'css-loader',
+                            options:{
+                                sourceMap:true
+                            }
+                        },
+                        {
+                            loader:'sass-loader',
+                            options:{
+                                sourceMap:true
+                            }
+                        }
+                    ]
+                })
             }]
         },
+        plugins:[
+            CSSExtract
+        ],
         devtool:isProduction ? 'source-map' : 'cheap-module-eval-source-map',
         devServer:{
             contentBase:path.join(__dirname,'public'),
